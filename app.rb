e66508af1a8e97b657f72321ccefb0898955c2d0
yeray <yerayrm90@gmail.com>
2012-12-30 18:26:02 +0000

    Acortador URL básico completado

---
+++ app.rb
@@ -0,0 +1,38 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'haml'
+
+set :database, 'sqlite3:///shortened_urls.db'
+#set :address, 'localhost:4567'
+set :address, 'exthost.etsii.ull.es:4567'
+
+class ShortenedUrl < ActiveRecord::Base
+  # Validates whether the value of the specified attributes are unique across the system.
+  validates_uniqueness_of :url
+  # Validates that the specified attributes are not blank
+  validates_presence_of :url
+  #validates_format_of :url, :with => /.*/
+  validates_format_of :url, 
+       :with => %r{^(https?|ftp)://.+}i, 
+       :allow_blank => true, 
+       :message => "The URL must start with http://, https://, or ftp:// ."
+end
+
+
+get '/' do
+  haml :index
+end
+
+post '/' do
+  @short_url = ShortenedUrl.find_or_create_by_url(params[:url])
+  if @short_url.valid?
+    haml :success, :locals => { :address => settings.address }
+  else
+    haml :index
+  end
+end
+
+get '/:shortened' do
+  short_url = ShortenedUrl.find(params[:shortened].to_i(36))
+  redirect short_url.url
+end
---
+++ db/migrate/20121230181420_shortened_urls.rb
@@ -0,0 +1,12 @@
+class ShortenedUrls < ActiveRecord::Migration
+  def up
+    create_table :shortened_urls do |t|
+      t.string :url
+    end
+    add_index :shortened_urls, :url
+  end
+
+  def down
+    drop_table :shortened_urls
+  end
+end
